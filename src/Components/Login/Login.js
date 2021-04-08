75b5d6706b5ebb7e6700726f4d086012d2650549
rawshad <rawshad@outlook.com>
2021-04-08 18:10:27 +0600

    kaj cholse

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import '../Login/Login.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './FirebaseConfigue';
 
+
+firebase.initializeApp(firebaseConfig);
 const Login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const googleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                console.log(result);
+                // var credential = result.credential;
+                // var token = credential.accessToken;
+                // var user = result.user;
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    }
     return (
         <div className="login__page">
             <h2 className="text-center">Login</h2>
             <form action="#" className="login__form">
-                <input type="email" placeholder="Enter Email" name="email" required/>
-                <input type="password" placeholder="Enter Password" name="password" required/>
+                <input type="email" placeholder="Enter Email" name="email" required />
+                <input type="password" placeholder="Enter Password" name="password" required />
                 <button>Login</button>
                 <button>Create Account</button>
-                <button>Continue With Google</button>
+                <button onClick={googleSignIn}>Continue With Google</button>
             </form>
         </div>
     );
